acd7db4e3780da98f0f2fda22ca9cf52231d495d
Eliana-2 <eliana.guizar1@gmail.com>
2023-07-05 19:28:37 -0700

    Add todoFactory

---
+++ modules/todoFactory.js
@@ -0,0 +1,19 @@
+function todoFactory(title, description, dueDate, priority, isComplete, notes) {
+  function getTitle() {return title};
+  function getDescription() {return description};
+  function getDueDate() {return dueDate};
+  function getPriority() {return priority};
+  function getIsComplete() {return isComplete};
+  function getNotes() {return notes};
+  function setTitle(newTitle) {title = newTitle}
+  function setDescription(newDescription) {description = newDescription};
+  function setDueDate(newDueDate) {dueDate = newDueDate};
+  function setPriority(newPriority) {priority = newPriority};
+  function setIsComplete(newIsComplete) {isComplete = newIsComplete};
+  function setNotes(newNotes) {notes = newNotes};
+
+  return {getTitle, getDescription, getDueDate, getPriority, getIsComplete, getNotes,
+          setTitle, setDescription, setDueDate, setPriority, setIsComplete, setNotes};
+}
+
+export {todoFactory}
