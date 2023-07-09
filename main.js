43af8032917f09b17e1c588e47a2384fc5bb05b0
Eliana-2 <eliana.guizar1@gmail.com>
2023-07-08 21:22:06 -0700

    Add function to display projects, styling

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 import './style.css';
+import {getProjects, addProject} from "./modules/Projects.js"
+import{displayProjects} from "./modules/DOM_Projects.js"
+
+
+addProject('Project 1');
+displayProjects(getProjects());
---
+++ modules/DOM_Projects.js
@@ -0,0 +1,31 @@
+function displayProjects(projects) {
+  const projectsNav = document.querySelector('.projects-nav');
+  projects.forEach((project) => {
+    const navElement = document.createElement('li');
+    navElement.classList.add('nav-element');
+
+    const navIcon = document.createElement('img');
+    navIcon.setAttribute('src', '../icons/text-box-check-outline.svg');
+    navIcon.classList.add('nav-icon');
+    navElement.appendChild(navIcon);
+
+    const navText = document.createElement('div');
+    navText.classList.add('nav-text');
+    navText.textContent = project.getProjectName();
+    navElement.appendChild(navText);
+
+    const editIcon = document.createElement('img');
+    editIcon.setAttribute('src', '../icons/file-edit-outline.svg');
+    editIcon.classList.add('nav-icon', 'edit');
+    navElement.appendChild(editIcon);
+
+    const deleteIcon = document.createElement('img');
+    deleteIcon.setAttribute('src', '../icons/trash-can-outline.svg');
+    deleteIcon.classList.add('nav-icon', 'delete');
+    navElement.appendChild(deleteIcon);
+
+    projectsNav.appendChild(navElement);
+  });
+}
+
+export {displayProjects}
